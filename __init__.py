55db76a58bbb870734c79a0a5c1eeedf70333d0c
João Pedro Martins de Souza Lima <joaopedrolima0203@gmail.com>
2026-08-26 00:15:23 -0300

    Módulo de MOEDAS.

--- __init__.py
+++ __init__.py
@@ -1,28 +1,42 @@
-def leiaDinheiro(msg):
-    valido = False
-    while not valido:
-        valor = input(msg).replace(',','.').strip()
-        if valor.isalpha() or valor == '':
-            print(f"\033[0;31mERRO! Insira apenas valores monetários! \"{valor}\" não é um deles.\033[m")
-        else:
-            valido = True
-            return float(valor)
+def metade(valor = 0, formatado = False):
+    resp = valor / 2
+    if formatado:
+        return format(resp)
+    else:
+        return resp
 
-def leiaInt(legenda): 
-    """
-    -> Funcionalidade que lê uma mensagem e retorna o valor APENAS se ele for um número inteiro válido
-    Args:
-        legenda (str): Legenda que vai aparecer na imagem
+def dobro(valor = 0, formatado = False):
+    resp = valor * 2
+    if formatado:
+        return format(resp)
+    else:
+        return resp
 
-    Returns:
-        int: valor inteiro digitado.
-    """
-    print("-" * 30)
-    while True:
-        n = input(legenda)
-        if n.isnumeric():
-            n = int(n)
-            break
-        else:
-            print("\033[0;31mERRO! Digite apenas números inteiros válidos.\033[m")
-    return n
+def aumentar(valor = 0, porcentagem = 0, formatado = False):
+    resp = valor + (valor * (porcentagem / 100))
+    if formatado:
+        return format(resp)
+    else:
+        return resp
+
+def diminuir(valor = 0, porcentagem = 0, formatado = False): 
+    resp = valor - (valor * (porcentagem / 100))
+    if formatado:
+        return format(resp)
+    else:
+        return resp
+
+def format(valor = 0.0, moeda = 'R$'):
+    return f"{moeda}{valor:.2f}".replace('.',',')
+
+def resumo(valor = 0, aumento = 0, diminui = 0):
+    print("=" * 35)
+    print(f"{'RESUMO DO VALOR':^35}")
+    print("=" * 35)
+    print(f"Preço analisado: \t{format(valor)}")
+    print(f"Dobro do preço: \t{dobro(valor, formatado=True)}")
+    print(f"Metade do preço: \t{metade(valor, formatado=True)}")
+    print(f"{aumento}% de aumento: \t{aumentar(valor, aumento, formatado=True)}")
+    print(f"{diminui}% de redução: \t{diminuir(valor, diminui, formatado=True)}")
+    print("=" * 35)
+    
