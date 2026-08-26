2ebee5142675317e8c627441f42c5e12f0be47e4
João Pedro Martins de Souza Lima <joaopedrolima0203@gmail.com>
2026-08-26 00:22:26 -0300

    Exercicio 113

---
+++ ex113.py
@@ -0,0 +1,4 @@
+import dados
+i = dados.leiaInt("Digite um número inteiro: ")
+n = dados.leiaNum("Digite um número real: ")
+print(f"O valor inteiro digitado foi {i} e o valor real digitado foi {n}.")
